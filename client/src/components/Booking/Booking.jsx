b55e3e63168bff48c1e3a2ee0b3314d2bf6afc35
janidu98 <janinduchathushan@gmail.com>
2024-01-25 00:40:38 +0530

    add booking form to booking  page

---
+++ client/src/components/Booking/Booking.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './booking.css'
+import { Form, FormGroup } from 'reactstrap';
+
+const Booking = ({tour, avgRating}) => {
+
+    const {price, reviews} = tour;
+
+    const handleChange = (e) => {
+
+    }
+
+  return (
+    <div className='booking'>
+      <div className="booking__top d-flex align-items-center justify-content-between">
+        <h3>${price} <span>/per person</span></h3>
+
+        <span className='tour__rating d-flex align-items-center'>
+                      <i class='ri-star-fill'></i> 
+                      {avgRating === 0 ? null : avgRating} ({reviews?.length})                    
+        </span>
+      </div>
+
+      {/* Booking form */}
+      <div className="booking__form">
+        <h5>Information</h5>
+        <Form className='booking__info-form'>
+            <FormGroup>
+                <input type="text" placeholder='Full Name' id='fullname' required onChange={handleChange} />
+            </FormGroup>
+            <FormGroup>
+                <input type="number" placeholder='Phone' id='phone' required onChange={handleChange} />
+            </FormGroup>
+            <FormGroup className='d-flex align-items-center gap-3'>
+                <input type="date" placeholder='' id='bookAt' required onChange={handleChange} />
+                <input type="number" placeholder='Guest Amount' id='guestSize' required onChange={handleChange} />
+            </FormGroup>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default Booking
--- client/src/pages/TourDetails.jsx
+++ client/src/pages/TourDetails.jsx
@@ -5,6 +5,7 @@ import {useParams} from 'react-router-dom'
 import tourData from '../assets/data/tours'
 import calculateAvgRating from '../utils/avgRating'
 import avatar from '../assets/images/avatar.jpg'
+import Booking from '../components/Booking/Booking'
 
 const TourDetails = () => {
 
@@ -43,7 +44,7 @@ const TourDetails = () => {
                 <div className="d-flex align-items-center gap-5">
                   <span className='tour__rating d-flex align-items-center gap-1'>
                       <i class='ri-star-fill' style={{color: "var(--secondary-color)"}}></i> 
-                      {calculateAvgRating === 0 ? null : avgRating} {totalRating === 0 ? 'Not rated' : (<span>({reviews?.length})</span>)}                      
+                      {avgRating === 0 ? null : avgRating} {totalRating === 0 ? 'Not rated' : (<span>({reviews?.length})</span>)}                      
                   </span>
 
                   <span><i class="ri-map-pin-user-fill"></i>{address}</span>
@@ -124,6 +125,10 @@ const TourDetails = () => {
                 </ListGroup>
               </div>
             </div>
+          </Col>
+
+          <Col lg="4">
+            <Booking tour={tour} avgRating={avgRating}/>
           </Col>
         </Row>
       </Container>
