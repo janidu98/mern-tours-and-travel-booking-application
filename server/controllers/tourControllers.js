ff14c0a9320bfd021f53aa27676b83a4f39d9e94
janidu98 <janinduchathushan@gmail.com>
2024-01-26 01:33:19 +0530

    create tour routes and controllers

--- server/controllers/tourControllers.js
+++ server/controllers/tourControllers.js
@@ -1,5 +1,7 @@
 import Tour from '../models/tourModel.js';
+import { errorHandler } from '../utils/error.js';
 
+//create new tour
 export const createTour = async(req, res, next) => {
     const newTour = new Tour(req.body);
 
@@ -7,6 +9,63 @@ export const createTour = async(req, res, next) => {
         const saveTour = await  newTour.save();
         return res.status(200).json(saveTour);
 
+    } catch (error) {
+        next(error);
+    }
+}
+
+//update tour
+export const updateTour = async (req, res, next) => {
+    const tour = await Tour.findById(req.params.id);
+    if(!tour) {
+        return next(errorHanlder(404, 'Tour is not found'));
+    }
+
+    try {
+        const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        return res.status(200).json(updatedTour);
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+//delete tour
+export const deleteTour = async (req, res, next) => {
+    const tour = await Tour.findById(req.params.id);
+    if(!tour) {
+        return next(errorHandler(404, 'Tour is not found'));
+    }
+
+    try {
+        await Tour.findByIdAndDelete(req.params.id);
+        return res.status(200).json('Tour has been deleted');
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+//get single tour
+export const getTour = async (req, res, next) => {
+    const tour = await Tour.findById(req.params.id);
+    if(!tour){
+        return next(errorHandler(404, 'Tour is not found'));
+    }
+
+    try {
+        return res.status(200).json(tour);
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+//get all tour
+export const getAllTour = async (req, res, next) => {
+    try {
+        const tours = await Tour.find({});
+        return res.status(200).json(tours);
     } catch (error) {
         next(error);
     }
--- server/routes/tourRoute.js
+++ server/routes/tourRoute.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import { createTour } from '../controllers/tourControllers.js';
+import { createTour, deleteTour, getAllTour, getTour, updateTour } from '../controllers/tourControllers.js';
 
 const router = express.Router();
 
-router.post('/', createTour);
+//create new tour
+router.post('/create', createTour);
+//update a tour
+router.put('/update/:id', updateTour);
+//delete a tour
+router.delete('/delete/:id', deleteTour)
+//get single tour
+router.get('/get/:id', getTour);
+//get all tour
+router.get('/get', getAllTour);
 
 export default router;
