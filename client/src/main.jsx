a9be7cb5adc3c019803f3cf81ea4aac397ce521c
janidu98 <janinduchathushan@gmail.com>
2024-02-04 04:12:37 +0530

    create authContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,12 +8,15 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material';
+import { AuthContextProvider } from './context/AuthContext.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>   
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AuthContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthContextProvider>
   </React.StrictMode>,
 )
